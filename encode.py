88d70890106f0dc9e374f5277fc30a27093f8983
Bit128DS <theodledropbox@gmail.com>
2023-01-22 18:42:41 +0100

    add byte decryption

--- encode.py
+++ encode.py
@@ -3,8 +3,6 @@ import zlib
 import codecs
 import struct
 
-inputbytes = b'\x90\x00\x03\x03\x90\x90\x00\x00\x00\x80\xe8\x07\x91\x90\x00\x00\x01\x80\xe8\x07\x91\x90\x00\x00\x02\x80\xe8\x07\x91\x91\x91'
-
 def encodeBytes(datalist:list) -> bytes:
     output = b'\x90'
     for data in datalist:
@@ -58,13 +56,27 @@ def encodeBytes(datalist:list) -> bytes:
                 output += b'\x8c'
                 output += length.to_bytes(4, "little")
             else:
-                raise OverflowError ("String ["+data+"] too long to represent (len>0xFFFFFFFF)")
+                raise OverflowError ("String ["+data+"] too long to represent (length>0xFFFFFFFF)")
             output += data.encode('utf8')
         elif type(data) == list:
             output += encodeBytes(data)
         elif type(data) == float:
             output += b'\x89'
             output += struct.pack("d", data)
+        elif type(data) == bytes:
+            length = len(data)
+            if length <= 0xFF:
+                output += b'\x94'
+                output += length.to_bytes(1, "little")
+            elif length <= 0xFFFF:
+                output += b'\x95'
+                output += length.to_bytes(2, "little")
+            elif length <= 0xFFFFFFFF:
+                output += b'\x96'
+                output += length.to_bytes(4, "little")
+            else:
+                raise OverflowError ("Bytes "+data+" too long to be represented")
+            output += data
         elif type(data) == bool:
             if data == True:
                 output += b'\x8D'
@@ -77,6 +89,14 @@ def encodeBytes(datalist:list) -> bytes:
     return output + b'\x91'
 
 inputList = [0, 11, 1, [[1, [0, 0, 'config_pusher', 0, [0, 0, 116.5999984741211, 20, False, 8.899999618530273], 'filter_items', 0, [0, 0, 0]]], [0, 9, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 63.400001525878906, 20, False, 8.899999618530273], 'filter_items', 0, [0, 0, 0]]], [0, 1, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 69.4000015258789, 20, False, 8.5], 'filter_items', 0, [0, 0, 0]]], [0, 2, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 58, 20, False, 9.399999618530273], 'filter_items', 0, [0, 0, 0]]], [0, 0, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 5224, False, 8.199999809265137], 'filter_items', 0, [0, 0, 0]]], [0, 7, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 5196, False, 8.199999809265137], 'filter_items', 0, [0, 0, 0]]], [0, 3, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 82.9000015258789, 20, False, 8.100000381469727], 'filter_items', 0, [0, 0, 0]]], [0, 4, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 5210, False, 8], 'filter_items', 0, [0, 0, 0]]], [0, 5, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 110.5999984741211, 20, False, 8.5], 'filter_items', 0, [0, 0, 0]]], [0, 8, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 97.4000015258789, 20, False, 8], 'filter_items', 0, [0, 0, 0]]], [0, 6, 0, 242], [1, [0, 0, 'config_pusher', 0, [0, 0, 5242, False, 9.399999618530273], 'filter_items', 0, [0, 0, 0]]], [0, 10, 0, 242]]]
+
+for i in range(len(inputList[3])):
+    command = inputList[3][i]
+    if command[0] == 1:
+        commandbytes = encodeBytes(command[1])#[1:-1]
+        inputList[3][i][1] = commandbytes
+
+print(inputList)
 
 inputbytes = encodeBytes(inputList)
 
